8bf79fb7d6a658ca08f47824e577efe394529b4f
djimenezunihumboldt <tu-email@example.com>
2026-05-10 18:29:55 -0400

    Temporarily enable production error output for debugging

--- restaurant-api/app/Config/Boot/production.php
+++ restaurant-api/app/Config/Boot/production.php
@@ -1,7 +1,7 @@
 <?php
 
-defined('CI_DEBUG') || define('CI_DEBUG', false);
+defined('CI_DEBUG') || define('CI_DEBUG', true);
 defined('CodeIgniter\\CI_DEBUG') || define('CodeIgniter\\CI_DEBUG', CI_DEBUG);
 
-error_reporting(E_ALL & ~E_DEPRECATED);
-ini_set('display_errors', '0');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
