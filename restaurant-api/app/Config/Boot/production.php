e6b83d0b9d21330aedce194dc23b83f1df73b5d3
djimenezunihumboldt <tu-email@example.com>
2026-05-10 18:27:24 -0400

    Add CI4 environment boot files

---
+++ restaurant-api/app/Config/Boot/production.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('CI_DEBUG') || define('CI_DEBUG', false);
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', '0');
---
+++ restaurant-api/app/Config/Boot/testing.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('CI_DEBUG') || define('CI_DEBUG', true);
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
